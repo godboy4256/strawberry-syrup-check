0d3b8335ef4db2519bbbd59c201c78daccba3c07
정민철 <jeongmincheol@jeongmincheol-ui-MacBookPro.local>
2022-10-13 19:07:38 +0900

    10.13 달력 컴포넌트 완성, 체크박스 컴포넌트 완성

--- frontend/src/components/inputs/dateInput.tsx
+++ frontend/src/components/inputs/dateInput.tsx
@@ -1,40 +1,45 @@
-import React, { useState } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import IMGDate from "../../assets/img/date_icon.svg";
+import IMGRedDirection from "../../assets/img/red_direction.svg";
 import PopUp from "../common/popUp";
 import "../../styles/date.css";
+import PublicDateCal from "../../../../share/dayCal";
+import SelectInput from "./selectInput";
 
-const _Date = () => {
-  const month = 10;
-  const year = 2022;
-  const days_count = new Date(year, month, 0).getDate();
-  const date_arr = new Array(new Date(year, month - 1, 1).getDay()).fill("");
+const dateCal = new PublicDateCal();
+
+const _DatePopUp = ({ currentDate }: { currentDate: number[] }) => {
+  const days_count = new Date(currentDate[0], currentDate[1], 0).getDate();
+  const [selectedDate, setSelectedDate] = useState<number>(currentDate[2]);
+  const date_arr = new Array(
+    new Date(currentDate[0], currentDate[1] - 1, 1).getDay()
+  ).fill("");
   const year_arr = [];
-
   for (let i = 0; i < days_count; i++) {
     date_arr.push(String(i + 1));
   }
   for (let j = 0; j < 10; j++) {
     year_arr.push(String(new Date().getFullYear() - j));
   }
-
   return (
     <div className="date_input_container">
-      <div className="date_input_header">2022년 7월 13일</div>
-      <div className="date_input_select">
-        <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-        </select>
+      <div className="date_input_header">
+        {currentDate[0]}년 {currentDate[1]}월 {currentDate[2]}일
+      </div>
+      <div id="date_input_controllbar">
+        <button id="date_prev_btn">
+          <img src={IMGRedDirection} alt="Date Prev Button" />
+        </button>
+        <div id="date_select_box">
+          <SelectInput type="normal" options={year_arr} />
+          <SelectInput
+            type="normal"
+            options={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+          />
+        </div>
+        <button id="date_next_btn">
+          <img src={IMGRedDirection} alt="Date Next Button" />
+        </button>
       </div>
       <div className="date_input_days fs_16">
         <div>일</div>
@@ -46,32 +51,59 @@ const _Date = () => {
         <div>토</div>
       </div>
       <div className="date_input_dates">
-        {date_arr?.map((el: string) => {
+        {date_arr?.map((el: string, idx: number) => {
           return (
-            <div key={el} className=" fs_16">
-              {el}
+            <div
+              key={String(el + Date.now()) + idx}
+              onClick={() => {
+                if (!el) return;
+                setSelectedDate(Number(el));
+              }}
+              className={`fs_16 ${Number(el) === selectedDate ? "select" : ""}`}
+            >
+              {Number(el) === selectedDate ? (
+                <div className="select_box fs_16">{el}</div>
+              ) : (
+                el
+              )}
             </div>
           );
         })}
+      </div>
+      <div id="date_buttons">
+        <button id="date_select_cancel" className="fs_14">
+          취소
+        </button>
+        <button id="date_select_ok" className="fs_14">
+          선택
+        </button>
       </div>
     </div>
   );
 };
 
 const DateInput = () => {
-  const [onDatePopUp, setOnDatePopUp] = useState(false);
+  const currentDate = dateCal.GetCurrentDate();
+  const [onDatePopUp, setOnDatePopUp] = useState<boolean>(false);
   const onClickOnDatePopUp = () => {
     setOnDatePopUp((prev) => !prev);
   };
   return (
     <>
       <div onClick={onClickOnDatePopUp} className="date_container">
-        <div className="date_value">{"1986년 01월 31일"}</div>
+        <div className="date_value">
+          {currentDate[0]}년 {currentDate[1]}월 {currentDate[2]}일
+        </div>
         <div className="date_icon">
           <img src={IMGDate} alt="Date Icon" />
         </div>
       </div>
-      {onDatePopUp && <PopUp contents={<_Date />} />}
+      {onDatePopUp && (
+        <PopUp
+          contents={<_DatePopUp currentDate={currentDate} />}
+          buttons="none"
+        />
+      )}
     </>
   );
 };
--- frontend/src/pages/mainPage.tsx
+++ frontend/src/pages/mainPage.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import CheckBoxInput from "../components/inputs/checkBoxInput";
 import DateInput from "../components/inputs/dateInput";
 import Header from "../components/layout/header";
 import "../styles/mainpage.css";
@@ -6,8 +7,9 @@ const MainPage = () => {
   return (
     <>
       {/* <Header title="딸기시럽" leftType="logo" /> */}
-      <DateInput />
-      <div id="main_page_container"></div>
+      <div id="main_page_container">
+        <CheckBoxInput options={["메뉴1", "메뉴2"]} type="box_type" />
+      </div>
     </>
   );
 };
