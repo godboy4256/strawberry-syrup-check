f3ddf42fc2577f1a9d1d3cee00f023adeba76e85
Harry Oh <osw0124@daum.net>
2022-10-14 18:06:36 +0900

    10.14 예술인 개별 입력 수급 인정 필터 작성

--- backend/routes/detail.ts
+++ backend/routes/detail.ts
@@ -104,6 +104,7 @@ export default function (fastify: FastifyInstance, options: any, done: any) {
 
             const age = Number(new Date().getFullYear) - Number(new Date(req.body.birth).getFullYear)
             if (new Date(`${new Date().getFullYear}-${birthArray[1]}-${birthArray[2]}`).getTime() >= new Date().getTime()) age - 1
+
             ////////////////////////////////////////////////////////////////////////////////////////////////// 예술인
             if (!req.body.isShort) {
                 const artWorkingDays = Math.floor(retiredDay.diff(enterDay, 'date', true) + 1) // 예술인은 유/무급 휴일 개념이 없으며 가입기간 전체를 피보험 단위기간으로 취급한다.
@@ -120,7 +121,7 @@ export default function (fastify: FastifyInstance, options: any, done: any) {
                         artRealDayPay,
                         receiveDay,
                         artRealMonthPay,
-                        severancePay: artWorkingYears >= 1 ? Math.ceil(((artDayAvgPay * 30) * artWorkingDays) / 365) : 0,
+                        severancePay: artWorkingYears >= 1 ? Math.ceil(((artDayAvgPay * 30) * artWorkingDays) / 365) : 0, // 예술인은 퇴직금이 없는 듯 하다
                         artWorkingDays,
                     }
                 } else {
@@ -147,6 +148,72 @@ export default function (fastify: FastifyInstance, options: any, done: any) {
         }
     )
 
+    fastify.post(
+        '/art/short',
+        {
+            schema: {
+                body: {
+                    type: 'object',
+                    required: ['retired', 'workCate', 'retireReason', 'birth', 'disabled'],
+                    properties: {
+                        retired: DefineParamInfo.retired,
+                        workCate: DefineParamInfo.workCate,
+                        retireReason: DefineParamInfo.retireReason,
+                        birth: DefineParamInfo.birth,
+                        disabled: DefineParamInfo.disabled,
+                        lastWorkDay: {type: 'string'} // 마지막 근무일
+                    }
+                }
+            }
+        },
+        (req:any, res) => {
+            const birthArray = req.body.birth.split('-')
+            const age = Number(new Date().getFullYear) - Number(new Date(req.body.birth).getFullYear)
+            if (new Date(`${new Date().getFullYear}-${birthArray[1]}-${birthArray[2]}`).getTime() >= new Date().getTime()) age - 1
+            const lastWorkDay = dayjs(req.body.lastWorkDay)
+
+
+
+
+            if(true){ // 개별입력인 경우
+                const when24Arr = lastWorkDay.subtract(24, 'month').format('YYYY-MM-DD').split('-').map(Number)
+                const when12Arr = lastWorkDay.subtract(12, 'month').format('YYYY-MM-DD').split('-').map(Number)
+
+                const sorted = data.sort((a,b) => {
+                    if(a.year < b.year) return 1;
+                    if(a.year > b.year) return -1;
+                    return 0;
+                });
+
+                let sumJoinMonth = 0
+
+                // 예술인 => 24개월 내에 피보험 단위기간이 9개월 이상인가?
+                sorted.map(v => {
+                    let sumLeftWorkDay = 0
+                    if (v.year >= when24Arr[0]) {        
+                        if (v.year === when24Arr[0]) {
+                            v.months.map(v => {
+                                if (v.month <= when24Arr[1]) {
+                                    v.workDay >= 11 ? sumJoinMonth += 1 : sumLeftWorkDay += v.workDay
+                                }
+                            })
+                        } else {
+                            v.months.map(v => {
+                                v.workDay >= 11 ? sumJoinMonth += 1 : sumLeftWorkDay += v.workDay
+                            })
+                        }
+                        sumJoinMonth += (sumLeftWorkDay / 22)
+                    }
+                })
+
+                if (Math.ceil(sumJoinMonth) >= 9) {
+                    console.log("permit")
+                }
+            }
+            return true
+        }
+    )
+
     done()
 }
 
@@ -159,3 +226,65 @@ function calArtPay(sumTwelveMonthSalary: number[], artWorkingDays: number) {
     
     return {artDayAvgPay, artRealDayPay, artRealMonthPay}
 }
+
+function artCheckPermit(when24Arr: number[], data: any) {
+    const sorted = data.sort((a: any,b: any) => {
+        if(a.year < b.year) return 1;
+        if(a.year > b.year) return -1;
+        return 0;
+    });
+
+    let sumJoinMonth = 0
+
+    sorted.map(v => {
+        let sumLeftWorkDay = 0
+        if (v.year >= when24Arr[0]) {        
+            if (v.year === when24Arr[0]) {
+                v.months.map(v => {
+                    if (v.month <= when24Arr[1]) {
+                        v.workDay >= 11 ? sumJoinMonth += 1 : sumLeftWorkDay += v.workDay
+                    }
+                })
+            } else {
+                v.months.map(v => {
+                    v.workDay >= 11 ? sumJoinMonth += 1 : sumLeftWorkDay += v.workDay
+                })
+            }
+            sumJoinMonth += (sumLeftWorkDay / 22)
+        }
+    })
+}
+
+const data = [
+    {
+        year: 2021,
+        months: [
+            {month: 2, workDay: 11, pay: 1000000},
+            {month: 6, workDay: 11, pay: 2000000},
+            {month: 7, workDay: 11, pay: 320000},
+            {month: 10, workDay: 11, pay: 1800000},
+            {month: 12, workDay: 11, pay: 200000}
+        ]
+    },
+    {
+        year: 2018,
+        months: [
+            {month:2, workDay: 10, pay: 100000},
+            {month: 6, workDay: 10, pay: 200000},
+            {month: 7, workDay: 12, pay: 320000},
+            {month: 8, workDay: 12, pay: 320000},
+            {month: 10, workDay: 26, pay: 1800000},
+            {month: 12, workDay: 10, pay: 200000}            
+        ]
+    },  
+    { 
+        year: 2022,              
+        months: [
+            {month: 1, workDay: 11, pay: 500000},
+            {month: 2, workDay: 30, pay: 2000000},
+            {month: 3, workDay: 11, pay: 1000000},
+            {month: 5, workDay: 15, pay: 200000},
+            {month: 6, workDay: 10, pay: 200000}            
+        ]
+    }
+]
