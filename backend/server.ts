4d70b3ee40fdc3e280b160107a5dc19e5193ed3a
Harry Oh <osw0124@daum.net>
2022-11-11 18:17:44 +0900

    chore: dynamic swagger test

--- backend/server.ts
+++ backend/server.ts
@@ -30,13 +30,21 @@ server.register(fastifySwagger, {
 	swagger: {
 		info: {
 			title: "Test swagger",
-			description: "testing the fastify swagger api",
+			description: "Testing the Fastify swagger API",
 			version: "0.1.0",
 		},
-		host: "localhost:8080/swagger",
+		externalDocs: {
+			url: "https://swagger.io",
+			description: "Find more info here",
+		},
+		host: "localhost",
 		schemes: ["http"],
 		consumes: ["application/json"],
 		produces: ["application/json"],
+		tags: [
+			{ name: "user", description: "User related end-points" },
+			{ name: "code", description: "Code related end-points" },
+		],
 	},
 });
 
