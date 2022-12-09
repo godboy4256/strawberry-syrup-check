bf0fa431a0218e3e10477984d8b4c8b3137b4775
Harry Oh <osw0124@daum.net>
2022-12-09 12:46:36 +0900

    refactor: 주석 삭제/수정, 함수 분리

---
+++ backend/routes/multi/function/checkBasicRequirements.ts
@@ -0,0 +1,22 @@
+import dayjs from "dayjs";
+import { DefinedParamErrorMesg } from "../../../share/validate";
+import { TmainData } from "../schema";
+
+export const checkBasicRequirements = (mainData: TmainData) => {
+	const mainRetiredDay = dayjs(mainData.retiredDay);
+
+	// 1. 신청일이 이직일로 부터 1년 초과 확인
+	const now = dayjs(new Date());
+	if (Math.floor(now.diff(mainRetiredDay, "day", true)) > 365)
+		return { succ: false, mesg: DefinedParamErrorMesg.expire };
+
+	// 2. mainData의 근로형태가 예술인 특고인경우 예술인 또는 특고로 3개월 이상 근무해야한다.
+	if (mainData.workCate === 2 || mainData.workCate === 3) {
+		if (mainData.workingDays < 90) return { succ: false, mesg: "예술인/특고로 3개월 이상 근무해야합니다" };
+	}
+	if (mainData.workCate === 4 || mainData.workCate === 5) {
+		if (mainData.workingDays < 3) return { succ: false, mesg: "단기 예술인/특고로 3개월 이상 근무해야합니다" };
+	}
+
+	return { succ: true };
+};
---
+++ backend/routes/multi/function/checkDuplicationAcquisition.ts
@@ -0,0 +1,82 @@
+import { TaddData, TmainData } from "../schema";
+
+const checkDuplicationDays = (mainData: TmainData, permitAddCandidates: TaddData[]) => {
+	let isDuplicate = false;
+
+	console.log("HI~~");
+	for (let i = 0; i < permitAddCandidates.length; i++) {
+		if (
+			permitAddCandidates[i].enterDay < mainData.enterDay &&
+			permitAddCandidates[i].retiredDay > mainData.enterDay
+		)
+			isDuplicate = true;
+
+		if (
+			permitAddCandidates[i].enterDay > mainData.enterDay &&
+			permitAddCandidates[i].retiredDay < mainData.retiredDay
+		)
+			isDuplicate = true;
+
+		if (
+			permitAddCandidates[i].enterDay > mainData.enterDay &&
+			permitAddCandidates[i].retiredDay > mainData.retiredDay
+		)
+			isDuplicate = true;
+	}
+
+	for (let i = 0; i < permitAddCandidates.length; i++) {
+		if (isDuplicate) break;
+		console.log(permitAddCandidates[i]);
+		for (let j = i + 1; j < permitAddCandidates.length; j++) {
+			if (
+				permitAddCandidates[j].enterDay < permitAddCandidates[i].enterDay &&
+				permitAddCandidates[j].retiredDay > permitAddCandidates[i].enterDay
+			)
+				isDuplicate = true;
+			if (
+				permitAddCandidates[j].enterDay > permitAddCandidates[i].enterDay &&
+				permitAddCandidates[j].retiredDay < permitAddCandidates[i].retiredDay
+			)
+				isDuplicate = true;
+			if (
+				permitAddCandidates[j].enterDay > permitAddCandidates[i].enterDay &&
+				permitAddCandidates[j].retiredDay > permitAddCandidates[i].retiredDay
+			)
+				isDuplicate = true;
+
+			console.log(permitAddCandidates[j]);
+		}
+	}
+
+	return isDuplicate;
+};
+
+export const checkDuplicationAcquisition = (mainData: TmainData, permitAddCandidates: TaddData[]) => {
+	let isDoubleAcquisition = false; // 이중취득 여부
+	let tempWorkCount = { count: 0, permitDays: 0 };
+	let artWorkCount = { count: 0, permitMonths: 0 };
+	let specialWorkCount = { count: 0, permitMonths: 0 }; // 특고
+
+	const artWorkCates = [2, 4];
+	const specialWorkCates = [3, 5];
+
+	permitAddCandidates.map((permitAddCandidate, idx, permitAddCandidates) => {
+		if (artWorkCates.includes(permitAddCandidate.workCate)) {
+			artWorkCount.count++;
+			artWorkCount.permitMonths += permitAddCandidate.permitDays;
+		} else if (specialWorkCates.includes(permitAddCandidate.workCate)) {
+			specialWorkCount.count++;
+			specialWorkCount.permitMonths += permitAddCandidate.permitDays;
+		} else {
+			tempWorkCount.count++;
+			tempWorkCount.permitDays += permitAddCandidate.permitDays;
+		}
+	});
+
+	const hasDuplicateDays = checkDuplicationDays(mainData, permitAddCandidates);
+
+	if ((hasDuplicateDays && tempWorkCount.count >= 1 && artWorkCount.count >= 1) || specialWorkCount.count >= 1)
+		isDoubleAcquisition = true;
+
+	return { isDoubleAcquisition, tempWorkCount, artWorkCount, specialWorkCount };
+};
