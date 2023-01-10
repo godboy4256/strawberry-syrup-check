5612123f10bfacc867c174aef15bd868d11c3277
Harry Oh <osw0124@daum.net>
2023-01-10 19:20:15 +0900

    fix: 모든 근로형태가 같이 사용하던 복수형 계산기 api에서 자영업자 분리

--- backend/routes/multi/multi.ts
+++ backend/routes/multi/multi.ts
@@ -33,25 +33,30 @@ export default function multiRoute(fastify: FastifyInstance, options: any, done:
 		if (!checkResult.succ) return { checkResult };
 
 		/////////////////////////////////////////////////////////// 자영업자 관련 조건 확인 필터
-		let tempAddDatas: TaddData[] = [];
-		if (mainData.workCate === 8) {
-			tempAddDatas = addDatas.filter((el) => el.workCate === 8 || el.workCate === 2 || el.workCate === 3);
-		}
-		if (mainData.workCate !== 2 && mainData.workCate !== 3 && mainData.workCate !== 8) {
-			tempAddDatas = addDatas.filter((el) => el.workCate !== 8);
-		}
-		addDatas = tempAddDatas;
-		console.log(tempAddDatas);
-		console.log(addDatas);
+		// let tempAddDatas: TaddData[] = [];
+		// if (mainData.workCate === 8) {
+		// 	tempAddDatas = addDatas.filter((el) => el.workCate === 8 || el.workCate === 2 || el.workCate === 3);
+		// }
+		const filteredAddDatas =
+			mainData.workCate !== 2 && mainData.workCate !== 3 && mainData.workCate !== 8
+				? addDatas.filter((el) => el.workCate !== 8)
+				: addDatas;
+		// if (mainData.workCate !== 2 && mainData.workCate !== 3 && mainData.workCate !== 8) {
+		// 	tempAddDatas = addDatas.filter((el) => el.workCate !== 8);
+		// }
+		// addDatas = tempAddDatas;
+		console.log(filteredAddDatas);
+		// console.log(tempAddDatas);
+		// console.log(addDatas);
 		///////////////////////////////////////////////////////////
 
 		// 2. 마지막 직장의 입사일과 전직장의 이직일 사이 기간이 3년을 초과하는 지 확인 & 다음 근로 정보가 3년을 초과하는 경우 가장 최근 근로 정보만 이용해서 계산
 		console.log("start" + 2);
-		if (addDatas.length === 0) {
+		if (filteredAddDatas.length === 0) {
 			res.statusCode = 204;
 			return { succ: true };
 		}
-		const secondRetiredDay = dayjs(addDatas[0].retiredDay);
+		const secondRetiredDay = dayjs(filteredAddDatas[0].retiredDay);
 		const diffMainToSecond = Math.floor(mainEnterDay.diff(secondRetiredDay, "day", true));
 		if (diffMainToSecond > 1095) {
 			res.statusCode = 204;
@@ -65,7 +70,7 @@ export default function multiRoute(fastify: FastifyInstance, options: any, done:
 
 		// 4.  18개월 또는 24개월 시점을 고려해서 기간내의 직장 필터
 		console.log("start" + 4);
-		const permitAddCandidates: TaddData[] = addDatas.filter((addData) =>
+		const permitAddCandidates: TaddData[] = filteredAddDatas.filter((addData) =>
 			dayjs(addData.retiredDay).isSameOrAfter(limitDay, "date")
 		);
 
@@ -83,16 +88,15 @@ export default function multiRoute(fastify: FastifyInstance, options: any, done:
 
 		// 7. 수급 인정/불인정 판단
 		console.log("start" + 7);
-		console.log(isDuplicateAcquisition && mainData.workCate !== 8);
-		const isPermit =
-			isDuplicateAcquisition && mainData.workCate !== 8
-				? doubleCasePermitCheck(
-						tempWorkCount.permitDays,
-						artWorkCount.permitMonths,
-						specialWorkCount.permitMonths,
-						mainData.workCate
-				  )
-				: commonCasePermitCheck(permitAddCandidates, mainData);
+		console.log(isDuplicateAcquisition);
+		const isPermit = isDuplicateAcquisition
+			? doubleCasePermitCheck(
+					tempWorkCount.permitDays,
+					artWorkCount.permitMonths,
+					specialWorkCount.permitMonths,
+					mainData.workCate
+			  )
+			: commonCasePermitCheck(permitAddCandidates, mainData);
 
 		// 8. 수급 불인정 조건에 맞는 경우 불인정 메세지 리턴
 		console.log("start" + 8);
@@ -115,7 +119,7 @@ export default function multiRoute(fastify: FastifyInstance, options: any, done:
 
 		// 9. 전체 피보험기간을 산정하기위한 합산 가능 유형 필터링
 		console.log("start" + 9);
-		const addCadiates = makeAddCadiates(addDatas, mainEnterDay);
+		const addCadiates = makeAddCadiates(filteredAddDatas, mainEnterDay);
 
 		// 10. 피보험기간 산정
 		console.log("start" + 10);
@@ -124,10 +128,7 @@ export default function multiRoute(fastify: FastifyInstance, options: any, done:
 
 		// 11. 소정급여일수 산정
 		console.log("start" + 11);
-		const tempReceiveDay =
-			mainData.workCate === 8
-				? getEmployerReceiveDay(workingYears)
-				: getReceiveDay(workingYears, mainData.age, mainData.disabled);
+		const tempReceiveDay = getReceiveDay(workingYears, mainData.age, mainData.disabled);
 		const receiveDay = addCadiates[addCadiates.length - 1].isIrregular
 			? tempReceiveDay === 120
 				? 120
@@ -202,7 +203,7 @@ export default function multiRoute(fastify: FastifyInstance, options: any, done:
 			};
 
 		// 9. 전체 피보험기간을 선정하기 위한 합산 가능 유형 필터링
-		const addCadiates = makeAddCadiates(addDatas, mainEnterDay);
+		const addCadiates = makeAddCadiates(filteredAddDatas, mainEnterDay);
 
 		// 10. 피보험기간 산정
 		const workingDays = mergeWorkingDays(mainData, addCadiates);
