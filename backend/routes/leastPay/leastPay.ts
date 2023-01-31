1ea75ee3ab3afe13e0d4c86245ac73aae72a5525
Harry Oh <osw0124@daum.net>
2023-01-31 15:31:21 +0900

    fix: 최저월급 게산기 결과값 변경
    - 최저 일급 추가

--- backend/routes/leastPay/leastPay.ts
+++ backend/routes/leastPay/leastPay.ts
@@ -11,16 +11,17 @@ export default function leastPayRoute(fastify: FastifyInstance, options: any, do
 			const workDay = req.body.workDay;
 			const weekWorkTime = workTime * workDay;
 			const leastTimePay = 9620;
-			const pay = req.body.pay < leastTimePay ? leastTimePay : req.body.pay;
+			// const pay = req.body.pay < leastTimePay ? leastTimePay : req.body.pay;
 
-			if (workTime >= 8 && workDay === 5) return 209 * pay;
+			if (workTime >= 8 && workDay === 5) return 209 * leastTimePay;
 
-			const commonPay = (Math.ceil(weekWorkTime * 4.345 * 10) / 10) * pay;
-			const addPay = (Math.ceil((weekWorkTime / 40) * 8 * 4.345 * 10) / 10) * pay;
+			const dayPay = leastTimePay * workTime;
+			const commonPay = (Math.ceil(weekWorkTime * 4.345 * 10) / 10) * leastTimePay;
+			const addPay = (Math.ceil((weekWorkTime / 40) * 8 * 4.345 * 10) / 10) * leastTimePay;
 			const isAddPay = weekWorkTime >= 15;
-			const result = isAddPay ? commonPay + addPay : commonPay;
-			console.log(commonPay, addPay, result);
-			return result;
+			const leastMonthPay = isAddPay ? commonPay + addPay : commonPay;
+			console.log(commonPay, addPay, leastMonthPay);
+			return { leastMonthPay, dayPay };
 		} catch (err) {
 			fastify.log.error(err);
 			return err;
