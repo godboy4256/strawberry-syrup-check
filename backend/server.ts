de74ba7977f7ab7ce80f6721d07990e68de5a1aa
Dolphin! <82381946+godboy4256@users.noreply.github.com>
2023-02-16 17:33:53 +0900

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -20,12 +20,9 @@ const server = fastify({
 	logger: false,
 });
 
-console.log("변경됨");
-
 server.register(cors, {
-	// origin: ["https://www.moneysylove.com", "https://www.moneysylove.du.r.appspot.com"],
-	// origin: ["http://localhost:8080/", "http://localhost:3000/"],
-	origin: true,
+	origin: ["https://www.moneysylove.com", "https://www.moneysylove.du.r.appspot.com"],
+	// origin: ["http://localhost:8080/", "http://localhost:3000/"]
 	methods: ["GET", "POST"],
 	credentials: true,
 });
