742b92cced7e770bbbd9edba725f8fa1e8716b68
Harry Oh <osw0124@daum.net>
2023-03-10 11:04:53 +0900

    feat: 프론트 파일 업데이트

--- backend/server.ts
+++ backend/server.ts
@@ -21,8 +21,7 @@ const server = fastify({
 });
 
 server.register(cors, {
-	// origin: ["https://www.moneysylove.com", "https://www.moneysylove.du.r.appspot.com"],
-	origin: true,
+	origin: ["https://www.moneysylove.com", "https://www.moneysylove.du.r.appspot.com"],
 	// origin: ["http://localhost:8080", "http://localhost:3000"],
 	methods: ["GET", "POST"],
 	credentials: true,
